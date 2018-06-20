cb7edd679bd401970ea996d25a7c47d987de2b9a
Alexander Ursu <alexander.ursu@gmail.com>
2018-06-21 01:48:56 +0200

    Removed extra spaces

--- manifests/init.pp
+++ manifests/init.pp
@@ -58,7 +58,6 @@ class nginx (
   # Primary Templates
   String $conf_template                                      = 'nginx/conf.d/nginx.conf.erb',
 
-  ### START Nginx Configuration ###
   ### START Nginx Configuration ###                                    # default:
   Optional[Boolean] $accept_mutex                            = undef,  # 'on' (nginx < 1.11.3)
                                                                        # 'off' (nginx >= 1.11.3)
@@ -69,38 +68,32 @@ class nginx (
   Optional[Nginx::Time] $send_timeout                        = undef,  # 60s
   Optional[Nginx::Time] $lingering_timeout                   = undef,  # 5s
   Optional[Nginx::Switch] $etag                              = undef,  # 'on'
-
   Optional[Nginx::ConnectionProcessing] $events_use          = undef,  # 'epoll'
   Optional[String] $fastcgi_cache_key                        = undef,  # undef
   Optional[Hash[String, Nginx::CachePath, 1]]
                     $fastcgi_cache_path                      = undef,  # undef
   Optional[Variant[Nginx::CacheUseStale, Array[Nginx::CacheUseStale]]]
                     $fastcgi_cache_use_stale                 = undef,  # 'off'
-  Boolean           $gzip                                    = true,   # 'on'
-  Optional[String]  $gzip_buffers                            = undef,  # '32 4k|16 8k'
+  Boolean $gzip                                              = true,   # 'on'
+  Optional[String] $gzip_buffers                             = undef,  # '32 4k|16 8k'
   Optional[Integer] $gzip_comp_level                         = undef,  # 1
   Optional[Variant[String, Array[String, 1]]] $gzip_disable  = undef,  # undef
   Optional[Integer] $gzip_min_length                         = undef,  # 20
-  Optional[Enum['1.0', '1.1']]
-                    $gzip_http_version                       = undef,  # '1.1'
-  Optional[Nginx::GzipProxied]
-                    $gzip_proxied                            = undef,  # 'off'
-  Optional[Variant[String, Array[String, 1]]]
-                    $gzip_types                              = undef,  # 'text/html'
+  Optional[Enum['1.0', '1.1']] $gzip_http_version            = undef,  # '1.1'
+  Optional[Nginx::GzipProxied] $gzip_proxied                 = undef,  # 'off'
+  Optional[Variant[String, Array[String, 1]]] $gzip_types    = undef,  # 'text/html'
   Optional[Boolean] $gzip_vary                               = undef,  # 'off'
   Optional[Nginx::ConfigSet] $http_cfg_prepend               = undef,
   Optional[Nginx::ConfigSet] $http_cfg_append                = undef,
   Optional[Variant[Array[String], String]] $http_raw_prepend = undef,
   Optional[Variant[Array[String], String]] $http_raw_append  = undef,
-  Optional[Boolean] $http_tcp_nodelay                        = undef,  # 'on'
-  Optional[Nginx::Switch] $http_tcp_nopush                         = undef,  # 'off'
+  Optional[Nginx::Switch] $http_tcp_nodelay                  = undef,  # 'on'
+  Optional[Nginx::Switch] $http_tcp_nopush                   = undef,  # 'off'
   Optional[Nginx::Time] $keepalive_timeout                   = undef,  # 75
   Optional[Integer] $keepalive_requests                      = undef,  # 100
-  Hash[String, String]
-                    $log_format                              = {},
+  Hash[String, String] $log_format                              = {},
   Boolean $mail                                              = false,
   Boolean $stream                                            = false,
-
   Optional[Boolean] $multi_accept                            = undef,  # 'off'
   Optional[Integer] $names_hash_bucket_size                  = undef,  # 32|64|128
   Optional[Integer] $names_hash_max_size                     = undef,  # 512
@@ -115,36 +108,33 @@ class nginx (
   Optional[Nginx::Time] $proxy_read_timeout                  = undef,  # 60
   Optional[String] $proxy_redirect                           = undef,  # 'default'
   Optional[Nginx::Time] $proxy_send_timeout                  = undef,  # 60
-  Array[String]     $proxy_set_header                        = [       # ['Host $proxy_host',
+  Array[String] $proxy_set_header                            = [       # ['Host $proxy_host',
       'Host $host',                                                    #  'Connection close']
       'X-Real-IP $remote_addr',
       'X-Forwarded-For $proxy_add_x_forwarded_for',
       'Proxy ""',
     ],
-  Array[String]     $proxy_hide_header                       = [],
-  Array[String]     $proxy_pass_header                       = [],
-  Array[String]     $proxy_ignore_header                     = [],
+  Array[String] $proxy_hide_header                           = [],
+  Array[String] $proxy_pass_header                           = [],
+  Array[String] $proxy_ignore_header                         = [],
   Optional[Boolean] $sendfile                                = undef,  # 'off'
-  Optional[Boolean] $server_tokens                           = undef,  # 'on',
-
+  Optional[Nginx::Switch] $server_tokens                     = undef,  # 'on',
   $spdy                                                      = 'off',
   $http2                                                     = 'off',
   $ssl_stapling                                              = 'off',
 
   Optional[Nginx::Size] $types_hash_bucket_size              = undef,  # 64
   Optional[Nginx::Size] $types_hash_max_size                 = undef,  # 1024
-  Integer           $worker_connections                      = 1024,   # 512
+  Integer $worker_connections                                = 1024,   # 512
   Optional[Nginx::Switch] $ssl_prefer_server_ciphers         = true,
   Variant[Enum['auto'], Integer] $worker_processes           = 'auto', # 1
   Optional[Integer] $worker_rlimit_nofile                    = undef,  # undef
-
   String $ssl_protocols                                      = 'TLSv1 TLSv1.1 TLSv1.2',
   String $ssl_ciphers                                        = 'ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:DHE-RSA-AES128-GCM-SHA256:DHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-AES128-SHA256:ECDHE-RSA-AES128-SHA256:ECDHE-ECDSA-AES128-SHA:ECDHE-RSA-AES256-SHA384:ECDHE-RSA-AES128-SHA:ECDHE-ECDSA-AES256-SHA384:ECDHE-ECDSA-AES256-SHA:ECDHE-RSA-AES256-SHA:DHE-RSA-AES128-SHA256:DHE-RSA-AES128-SHA:DHE-RSA-AES256-SHA256:DHE-RSA-AES256-SHA:ECDHE-ECDSA-DES-CBC3-SHA:ECDHE-RSA-DES-CBC3-SHA:EDH-RSA-DES-CBC3-SHA:AES128-GCM-SHA256:AES256-GCM-SHA384:AES128-SHA256:AES256-SHA256:AES128-SHA:AES256-SHA:DES-CBC3-SHA:!DSS', # lint:ignore:140chars
   Optional[Stdlib::Unixpath] $ssl_dhparam                    = undef,
-  Optional[Nginx::FileCache]
-                    $open_file_cache                         = undef,  # 'off'
-  Nginx::Time       $open_file_cache_valid                   = 60,
-  Integer           $open_file_cache_min_uses                = 1,
+  Optional[Nginx::FileCache] $open_file_cache                = undef,  # 'off'
+  Nginx::Time $open_file_cache_valid                         = 60,
+  Integer $open_file_cache_min_uses                          = 1,
   Optional[
     Hash[
       String,
@@ -156,11 +146,10 @@ class nginx (
     ]
   ]                 $limit_req_zone                          = undef,
   Optional[Boolean] $proxy_cache_lock                        = undef,  # 'off'
-
-  Optional[String]  $default_type                            = undef,  # 'text/plain'
-  Optional[String]  $charset_types                           = undef,  # 'text/html text/xml text/plain text/vnd.wap.wml application/javascript application/rss+xml'
-  Optional[String]  $charset                                 = undef,  # 'off'
-  Optional[String]  $index                                   = undef,  # 'index.html'
+  Optional[String] $default_type                             = undef,  # 'text/plain'
+  Optional[String] $charset_types                            = undef,  # 'text/html text/xml text/plain text/vnd.wap.wml application/javascript application/rss+xml'
+  Optional[String] $charset                                  = undef,  # 'off'
+  Optional[String] $index                                    = undef,  # 'index.html'
   Optional[Boolean] $msie_padding                            = undef,  # 'on'
   Optional[Boolean] $port_in_redirect                        = undef,  # 'on'
   Optional[Nginx::Time] $client_header_timeout               = undef,  # 60s
