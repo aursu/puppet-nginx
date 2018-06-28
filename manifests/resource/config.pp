b71632bb1865b30a8bc9b8b543ac4cf5924af47b
Alexander Ursu <alexander.ursu@gmail.com>
2018-06-28 23:51:40 +0200

    Added logic into defined type nginx::resource::config

--- manifests/resource/config.pp
+++ manifests/resource/config.pp
@@ -4,6 +4,30 @@
 #
 # @example
 #   nginx::resource::config { 'namevar': }
-define nginx::resource::config(
+define nginx::resource::config (
+  String  $template,
+  String  $filename = $name,
+  Stdlib::Unixpath
+          $conf_dir = $nginx::conf_dir,
 ) {
+  if ! defined(Class['nginx']) {
+    fail('You must include the nginx base class before using any defined resources')
+  }
+
+  $pathdata = split($filename, '/')
+  $basename = $pathdata[-1]
+  $namedata = split($basename, '.')
+
+  if $namedata[-1] == 'conf' {
+    $configname = $basename
+  }
+  else {
+    $configname = "${basename}.conf"
+  }
+
+  file { "${conf_dir}/conf.d/${configname}":
+    ensure  => file,
+    content => template($template),
+    require => File["${conf_dir}/conf.d"],
+  }
 }
