a64d1a913673ddcc16f16bdd9282286f8a41054a
Alexander Ursu <alexander.ursu@gmail.com>
2020-10-15 20:10:22 +0300

    Fixed rubocop issues

--- spec/defines/resource_config_spec.rb
+++ spec/defines/resource_config_spec.rb
@@ -4,7 +4,7 @@ describe 'nginx::resource::config' do
   let(:title) { 'namevar' }
   let(:params) do
     {
-      template: Dir.pwd + '/spec/fixtures/files/hsts.epp',
+      template: Dir.pwd + '/spec/fixtures/files/hsts.epp'
     }
   end
   let(:pre_condition) { ['include ::nginx'] }
@@ -16,26 +16,24 @@ describe 'nginx::resource::config' do
       it { is_expected.to compile }
 
       it {
-        is_expected.to contain_file('/etc/nginx/conf.d/namevar.conf')
-          .with_ensure('file')
-          .with_content(%r{"" "max-age=15768000";})
-          .that_requires('File[/etc/nginx/conf.d]')
-          .that_notifies('Service[nginx]')
+        is_expected.to contain_file('/etc/nginx/conf.d/namevar.conf').with(
+          'ensure' => 'file',
+          'content' => %r{"" "max-age=15768000";}
+        ).that_requires('File[/etc/nginx/conf.d]').that_notifies('Service[nginx]')
       }
 
-      context "when content is provided" do
+      context 'when content is provided' do
         let :params do
           {
-            content: '<html><body>It works!</body></html>',
+            content: '<html><body>It works!</body></html>'
           }
         end
 
         it {
-          is_expected.to contain_file('/etc/nginx/conf.d/namevar.conf')
-            .with_ensure('file')
-            .with_content('<html><body>It works!</body></html>')
-            .that_requires('File[/etc/nginx/conf.d]')
-            .that_notifies('Service[nginx]')
+          is_expected.to contain_file('/etc/nginx/conf.d/namevar.conf').with(
+            'ensure' => 'file',
+            'content' => '<html><body>It works!</body></html>'
+          ).that_requires('File[/etc/nginx/conf.d]').that_notifies('Service[nginx]')
         }
       end
     end
