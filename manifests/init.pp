1ab91ad9821f4510609f4203839626e926e82f5a
Oleksandr Ursu <alexander.ursu@gmail.com>
2024-04-26 21:11:17 +0200

    Corrected default parameter for Nginx version

--- manifests/init.pp
+++ manifests/init.pp
@@ -616,7 +616,7 @@ class nginx (
   Hash $nginx_upstreams                                   = {},
   Nginx::UpstreamDefaults $nginx_upstreams_defaults       = {},
   Boolean $purge_passenger_repo                           = true,
-  String[1] $nginx_version                                = pick(fact('nginx_version'), '1.14.0'),
+  String[1] $nginx_version                                = pick($facts['nginx_version'], '1.14.0'),
 
   ### END Hiera Lookups ###
 ) inherits nginx::params {
